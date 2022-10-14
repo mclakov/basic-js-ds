48af86535510f3d4af099963b46580a554627a28
Roman Maklakov <mclakov@MR-MacBook.local>
2022-10-15 00:27:24 +0300

    feat: remove-from-list task done

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+
+
+  // console.log('l = ', l.ListNode);
+  // console.log('l.value = ', l.next.next);
+
+  while (l.value === k) {
+    l = l.next;
+  }
+
+  let currentNode = l;
+
+    while (currentNode.next) {
+      if (currentNode.next.value === k) {
+        currentNode.next = currentNode.next.next;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+
+  return l;
 }
 
 module.exports = {
