db11848b75268e6d8c0906411992c1c25d785e2e
Roman Maklakov <mclakov@MR-MacBook.local>
2022-10-15 22:32:02 +0300

    feat: binary-search-tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,22 +1,64 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
+
+//
+//   class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+
+    if(!this.rootNode) {
+      this.rootNode = newNode;
+    }
+
+    let currentNode = this.rootNode;
+    while (currentNode) {
+      if (newNode.data < currentNode.data) {
+        if (currentNode.left) {
+          currentNode.left = newNode;
+        }
+        currentNode = currentNode.left;
+      } else {
+        currentNode = currentNode.right;
+      }
+    }
   }
+
+
+
+  traverse(node, callback) {
+    if (!node) {
+      return;
+    }
+    if(callback){
+      callback(node);
+    }
+
+  }
+
+
+
+
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
